5dff03089debc095311fd112abc36b6c115abc49
fsmith26 <44277586+fsmith26@users.noreply.github.com>
2018-12-03 16:41:55 -0500

    Update query2.php

--- query2.php
+++ query2.php
@@ -1,4 +1,4 @@
-<?php/*Literally just a php file that creates radio buttons of different ways to sort. Used in task 2 */
+<?php //Literally just a php file that creates radio buttons of different ways to sort. Used in task 2
 echo '<input type="radio" name="action" value="priceAsc">Price-Asc<br>'; 	//all radio buttons are used in a form to tell php how to sort
 echo '<input type="radio" name="action" value="priceDesc">Price-Desc<br>';
 echo '<input type="radio" name="action" value="descAsc">Description-Asc<br>';
